3d62c666aa6b6b9c53969e5c0e3168f0844ab04e
Paulo Roberto <paulodiastst@hotmail.com>
2021-12-03 22:42:16 -0300

    Escutando Eventos

--- README.md
+++ README.md
@@ -2,10 +2,9 @@
  
 > <h1>Criando um Aplicativo que Lembra o Usuário de Beber Água</h1>
 
-<p align="center"> Em desenvolvimento ⚠️  </p>
+## - Configurando layout xml ScrollView. 
+## - Modificação do codigo Java no MainActivity.
+## - Definindo som dos eventos do app.
 
 
-<p align="center">
-<img src="https://user-images.githubusercontent.com/79487813/144345593-10ed5a88-97f3-46f6-be53-d364fadf414d.png"/>
-</P>
 
--- app/src/main/java/com/paulodias/beberagua/MainActivity.java
+++ app/src/main/java/com/paulodias/beberagua/MainActivity.java
@@ -3,12 +3,42 @@ package com.paulodias.beberagua;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TimePicker;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button btnNotify;
+    private EditText editMinutes;
+    private TimePicker timePicker;
+
+    private int hour;
+    private int minute;
+    private int interval;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnNotify = findViewById(R.id.btn_notify);
+        editMinutes = findViewById(R.id.edit_txt_number_interval);
+        timePicker = findViewById(R.id.time_picker);
+
+        timePicker.setIs24HourView(true);
+    }
+
+    // EVENTO DE CLICK VIA XML
+    public void notifyClick(View view) {
+       String sInterval = editMinutes.getText().toString();
+
+       hour = timePicker.getChildCount();
+       minute = timePicker.getCurrentMinute();
+       interval = Integer.parseInt(sInterval);
+
+        Log.d("teste", "hora: " + hour + " minuto: " + minute + " intervalo: " + interval);
     }
 }
